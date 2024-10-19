a33b06cce4368cd0f9d1ece1d0a12a466633982f
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-10-19 17:23:28 -0300

    Create Config.cs

---
+++ Manager/Config.cs
@@ -0,0 +1,27 @@
+﻿namespace Manager
+{
+    internal class Config
+    {
+        public List<Server> Servers;
+        public List<Pipeline> Pipelines;
+    }
+
+    public class Server
+    {
+        public Guid Id;
+        public string Name;
+        public string Host;
+        public short Port;
+    }
+
+    public class Pipeline
+    {
+        public string Name;
+        public List<Step> Steps;
+    }
+
+    public class Step
+    {
+        public string Name;
+    }
+}
