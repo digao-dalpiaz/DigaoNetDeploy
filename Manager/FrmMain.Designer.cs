5dce3d1d2dc9f7a9d6df67cfbf8daead41b2dd61
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-10-19 17:29:58 -0300

    tool bar and images

--- Manager/FrmMain.Designer.cs
+++ Manager/FrmMain.Designer.cs
@@ -28,18 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            ToolBar = new ToolStrip();
+            BtnServers = new ToolStripButton();
+            BtnPipelines = new ToolStripButton();
+            ToolBar.SuspendLayout();
             SuspendLayout();
+            // 
+            // ToolBar
+            // 
+            ToolBar.ImageScalingSize = new Size(32, 32);
+            ToolBar.Items.AddRange(new ToolStripItem[] { BtnServers, BtnPipelines });
+            ToolBar.Location = new Point(0, 0);
+            ToolBar.Name = "ToolBar";
+            ToolBar.Size = new Size(800, 39);
+            ToolBar.TabIndex = 0;
+            ToolBar.Text = "toolStrip1";
+            // 
+            // BtnServers
+            // 
+            BtnServers.Image = Properties.Resources.server;
+            BtnServers.ImageTransparentColor = Color.Magenta;
+            BtnServers.Name = "BtnServers";
+            BtnServers.Size = new Size(92, 36);
+            BtnServers.Text = "Servers";
+            // 
+            // BtnPipelines
+            // 
+            BtnPipelines.Image = Properties.Resources.pipeline;
+            BtnPipelines.ImageTransparentColor = Color.Magenta;
+            BtnPipelines.Name = "BtnPipelines";
+            BtnPipelines.Size = new Size(104, 36);
+            BtnPipelines.Text = "Pipelines";
             // 
             // FrmMain
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(ToolBar);
             Name = "FrmMain";
             Text = "Digao Net Deploy - Manager";
+            ToolBar.ResumeLayout(false);
+            ToolBar.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ToolStrip ToolBar;
+        private ToolStripButton BtnServers;
+        private ToolStripButton BtnPipelines;
     }
 }
