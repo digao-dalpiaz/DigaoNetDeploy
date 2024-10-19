a2079523bed1286fe90accd0233efac999a88477
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-10-19 17:54:11 -0300

    server form design

--- Manager/FrmServers.Designer.cs
+++ Manager/FrmServers.Designer.cs
@@ -28,22 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            listBox1 = new ListBox();
+            toolStrip1 = new ToolStrip();
+            BtnAdd = new ToolStripButton();
+            BtnEdit = new ToolStripButton();
+            BtnDelete = new ToolStripButton();
+            toolStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Fill;
+            listBox1.FormattingEnabled = true;
+            listBox1.IntegralHeight = false;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(0, 25);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(420, 313);
+            listBox1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { BtnAdd, BtnEdit, BtnDelete });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(420, 25);
+            toolStrip1.TabIndex = 1;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // BtnAdd
+            // 
+            BtnAdd.Image = Properties.Resources.add;
+            BtnAdd.ImageTransparentColor = Color.Magenta;
+            BtnAdd.Name = "BtnAdd";
+            BtnAdd.Size = new Size(51, 22);
+            BtnAdd.Text = "New";
+            // 
+            // BtnEdit
+            // 
+            BtnEdit.Image = Properties.Resources.edit;
+            BtnEdit.ImageTransparentColor = Color.Magenta;
+            BtnEdit.Name = "BtnEdit";
+            BtnEdit.Size = new Size(47, 22);
+            BtnEdit.Text = "Edit";
+            // 
+            // BtnDelete
+            // 
+            BtnDelete.Image = Properties.Resources.delete;
+            BtnDelete.ImageTransparentColor = Color.Magenta;
+            BtnDelete.Name = "BtnDelete";
+            BtnDelete.Size = new Size(70, 22);
+            BtnDelete.Text = "Remove";
             // 
             // FrmServers
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(700, 338);
+            ClientSize = new Size(420, 338);
+            Controls.Add(listBox1);
+            Controls.Add(toolStrip1);
             Margin = new Padding(3, 2, 3, 2);
             MinimizeBox = false;
             Name = "FrmServers";
             ShowInTaskbar = false;
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Servers";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private ToolStrip toolStrip1;
+        private ToolStripButton BtnAdd;
+        private ToolStripButton BtnEdit;
+        private ToolStripButton BtnDelete;
     }
 }
--- Manager/Properties/Resources.Designer.cs
+++ Manager/Properties/Resources.Designer.cs
@@ -63,6 +63,36 @@ namespace Manager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap add {
+            get {
+                object obj = ResourceManager.GetObject("add", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap delete {
+            get {
+                object obj = ResourceManager.GetObject("delete", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap edit {
+            get {
+                object obj = ResourceManager.GetObject("edit", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap pipeline {
             get {
                 object obj = ResourceManager.GetObject("pipeline", resourceCulture);
