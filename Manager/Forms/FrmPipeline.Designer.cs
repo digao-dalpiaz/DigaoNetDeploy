c54dfa925f6c286997f458c5fcacb27998f7fa67
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-10-20 14:23:54 -0300

    pipeline save

--- Manager/Forms/FrmPipeline.Designer.cs
+++ Manager/Forms/FrmPipeline.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            BtnCancel = new Button();
+            BtnOK = new Button();
+            EdName = new TextBox();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // BtnCancel
+            // 
+            BtnCancel.DialogResult = DialogResult.Cancel;
+            BtnCancel.Location = new Point(328, 296);
+            BtnCancel.Name = "BtnCancel";
+            BtnCancel.Size = new Size(104, 32);
+            BtnCancel.TabIndex = 12;
+            BtnCancel.Text = "Cancel";
+            BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // BtnOK
+            // 
+            BtnOK.Location = new Point(224, 296);
+            BtnOK.Name = "BtnOK";
+            BtnOK.Size = new Size(96, 32);
+            BtnOK.TabIndex = 11;
+            BtnOK.Text = "OK";
+            BtnOK.UseVisualStyleBackColor = true;
+            BtnOK.Click += BtnOK_Click;
+            // 
+            // EdName
+            // 
+            EdName.Location = new Point(16, 32);
+            EdName.Name = "EdName";
+            EdName.Size = new Size(400, 23);
+            EdName.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 10;
+            label1.Text = "Name";
             // 
             // FrmPipeline
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(BtnCancel);
+            Controls.Add(BtnOK);
+            Controls.Add(EdName);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             MaximizeBox = false;
             MinimizeBox = false;
@@ -42,9 +86,16 @@
             ShowInTaskbar = false;
             StartPosition = FormStartPosition.CenterScreen;
             Text = "New Pipeline";
+            Load += FrmPipeline_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button BtnCancel;
+        private Button BtnOK;
+        private TextBox EdName;
+        private Label label1;
     }
 }
--- Manager/Forms/FrmPipeline.cs
+++ Manager/Forms/FrmPipeline.cs
@@ -1,15 +1,42 @@
 ﻿using Manager.Storage;
+using Manager.Utility;
 
 namespace Manager
 {
     public partial class FrmPipeline : Form
     {
 
-        public Pipeline ReturningObj;
+        public Pipeline ReturningObj { get; set; }
 
         public FrmPipeline()
         {
             InitializeComponent();
+        }
+
+        private void FrmPipeline_Load(object sender, EventArgs e)
+        {
+            if (ReturningObj != null)
+            {
+                Text = "Edit Pipeline";
+
+                EdName.Text = ReturningObj.Name;
+            }
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            if (!Messages.ValidateField(EdName, "Name")) return;
+
+            //
+
+            if (ReturningObj == null)
+            {
+                ReturningObj = new();
+            }
+
+            ReturningObj.Name = EdName.Text;
+
+            DialogResult = DialogResult.OK;
         }
     }
 }
