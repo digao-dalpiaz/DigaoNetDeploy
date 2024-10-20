42df55b49ef6a1f8024f40470a53e0a3fdf33e71
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-10-20 18:52:56 -0300

    Update UCPipelines.cs

--- Manager/Forms/UCPipelines.cs
+++ Manager/Forms/UCPipelines.cs
@@ -100,7 +100,7 @@ namespace Manager
                 }
                 catch (Exception ex)
                 {
-
+                    LogService.Log("ERROR: " + ex.Message, Color.Crimson);
                 }
                 
                 Invoke(() =>
