4bceb6616c167da35618fd93386e0ec5ee230273
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-10-20 16:45:56 -0300

    Update FrmStep.Designer.cs

--- Manager/Forms/FrmStep.Designer.cs
+++ Manager/Forms/FrmStep.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            EdName = new TextBox();
+            label1 = new Label();
+            BtnCancel = new Button();
+            BtnOK = new Button();
             SuspendLayout();
+            // 
+            // EdName
+            // 
+            EdName.Location = new Point(16, 32);
+            EdName.Name = "EdName";
+            EdName.Size = new Size(400, 23);
+            EdName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Name";
+            // 
+            // BtnCancel
+            // 
+            BtnCancel.DialogResult = DialogResult.Cancel;
+            BtnCancel.Location = new Point(400, 328);
+            BtnCancel.Name = "BtnCancel";
+            BtnCancel.Size = new Size(104, 32);
+            BtnCancel.TabIndex = 10;
+            BtnCancel.Text = "Cancel";
+            BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // BtnOK
+            // 
+            BtnOK.Location = new Point(296, 328);
+            BtnOK.Name = "BtnOK";
+            BtnOK.Size = new Size(96, 32);
+            BtnOK.TabIndex = 9;
+            BtnOK.Text = "OK";
+            BtnOK.UseVisualStyleBackColor = true;
             // 
             // FrmStep
             // 
+            AcceptButton = BtnOK;
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = BtnCancel;
             ClientSize = new Size(800, 450);
+            Controls.Add(BtnCancel);
+            Controls.Add(BtnOK);
+            Controls.Add(EdName);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             MaximizeBox = false;
             MinimizeBox = false;
@@ -43,8 +88,14 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "New Step";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox EdName;
+        private Label label1;
+        private Button BtnCancel;
+        private Button BtnOK;
     }
 }
