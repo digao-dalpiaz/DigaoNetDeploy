135149d8184e2486f90ceea3b334853594565af4
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-10-20 16:24:27 -0300

    new linux connected image

--- Manager/Properties/Resources.Designer.cs
+++ Manager/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace Manager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap linux_connected {
+            get {
+                object obj = ResourceManager.GetObject("linux_connected", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap pipeline_blue {
             get {
                 object obj = ResourceManager.GetObject("pipeline_blue", resourceCulture);
