b441840fd78a437f233cd671c5f6a1111ba31599
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-10-20 11:51:25 -0300

    pipelines toolstrip and list

--- Manager/UCPipelines.Designer.cs
+++ Manager/UCPipelines.Designer.cs
@@ -28,17 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            List = new ListBox();
+            ToolBar = new ToolStrip();
+            BtnAdd = new ToolStripButton();
+            BtnEdit = new ToolStripButton();
+            BtnDelete = new ToolStripButton();
+            ToolBar.SuspendLayout();
             SuspendLayout();
+            // 
+            // List
+            // 
+            List.BorderStyle = BorderStyle.None;
+            List.Dock = DockStyle.Fill;
+            List.DrawMode = DrawMode.OwnerDrawFixed;
+            List.FormattingEnabled = true;
+            List.IntegralHeight = false;
+            List.ItemHeight = 48;
+            List.Location = new Point(0, 31);
+            List.Name = "List";
+            List.Size = new Size(656, 385);
+            List.TabIndex = 3;
+            // 
+            // ToolBar
+            // 
+            ToolBar.ImageScalingSize = new Size(24, 24);
+            ToolBar.Items.AddRange(new ToolStripItem[] { BtnAdd, BtnEdit, BtnDelete });
+            ToolBar.Location = new Point(0, 0);
+            ToolBar.Name = "ToolBar";
+            ToolBar.Size = new Size(656, 31);
+            ToolBar.TabIndex = 2;
+            // 
+            // BtnAdd
+            // 
+            BtnAdd.Image = Properties.Resources.add;
+            BtnAdd.ImageTransparentColor = Color.Magenta;
+            BtnAdd.Name = "BtnAdd";
+            BtnAdd.Size = new Size(59, 28);
+            BtnAdd.Text = "New";
+            // 
+            // BtnEdit
+            // 
+            BtnEdit.Image = Properties.Resources.edit;
+            BtnEdit.ImageTransparentColor = Color.Magenta;
+            BtnEdit.Name = "BtnEdit";
+            BtnEdit.Size = new Size(55, 28);
+            BtnEdit.Text = "Edit";
+            // 
+            // BtnDelete
+            // 
+            BtnDelete.Image = Properties.Resources.delete;
+            BtnDelete.ImageTransparentColor = Color.Magenta;
+            BtnDelete.Name = "BtnDelete";
+            BtnDelete.Size = new Size(78, 28);
+            BtnDelete.Text = "Remove";
             // 
             // UCPipelines
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(List);
+            Controls.Add(ToolBar);
             Name = "UCPipelines";
             Size = new Size(656, 416);
+            ToolBar.ResumeLayout(false);
+            ToolBar.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox List;
+        private ToolStrip ToolBar;
+        private ToolStripButton BtnAdd;
+        private ToolStripButton BtnEdit;
+        private ToolStripButton BtnDelete;
     }
 }
