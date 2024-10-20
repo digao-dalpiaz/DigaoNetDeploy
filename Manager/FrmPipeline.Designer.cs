3dd485691f62ee925f63bd48e740fdb08313fc8d
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-10-20 12:02:25 -0300

    pipeline form design

--- Manager/FrmPipeline.Designer.cs
+++ Manager/FrmPipeline.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmPipeline";
+            SuspendLayout();
+            // 
+            // FrmPipeline
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FrmPipeline";
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "New Pipeline";
+            ResumeLayout(false);
         }
 
         #endregion
