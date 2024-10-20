a5b6f6e190a9d4ce2d13f3186ceff4071e074e2a
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-10-20 17:06:32 -0300

    Update FrmPipeline.cs

--- Manager/Forms/FrmPipeline.cs
+++ Manager/Forms/FrmPipeline.cs
@@ -98,7 +98,7 @@ namespace Manager
             if (e.Index == -1) return;
             var step = Steps.Items[e.Index] as Step;
 
-            DrawItemEx.Draw(e, Properties.Resources.step, step.Name, Color.Black, Color.Gold);
+            DrawItemEx.Draw(e, Properties.Resources.step, step.Name, Color.Black, Color.FromArgb(242, 242, 99));
         }
 
         private void Steps_DoubleClick(object sender, EventArgs e)
