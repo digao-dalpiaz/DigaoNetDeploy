7692c7dd1203d6ed5c92708094ca33dff5d3a57b
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2024-10-20 11:51:56 -0300

    Update UCPipelines.cs

--- Manager/UCPipelines.cs
+++ Manager/UCPipelines.cs
@@ -1,10 +1,16 @@
-﻿namespace Manager
+﻿using Manager.Theme;
+
+namespace Manager
 {
     public partial class UCPipelines : UserControl
     {
         public UCPipelines()
         {
             InitializeComponent();
+
+            MyToolStripRenderer.ConfigToolStrip(ToolBar);
+
+            List.BackColor = Vars.BACKGROUND;
         }
     }
 }
